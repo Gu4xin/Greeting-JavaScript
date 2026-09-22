b8014a03da6a3b4b492fa78ca2692a9bce6f560a
Guaxin <brenoyohan@yahoo.com>
2026-09-21 22:18:33 -0300

    Conseui fazer as mensagens personalizadas funcionarem. Agora ele funciona, só precisa de polimento pra ficar mais bonitinho e coisas mais bobas.

--- Fundamentos do JavaScript/Scripts/script.js
+++ Fundamentos do JavaScript/Scripts/script.js
@@ -1,15 +1,18 @@
+const areaTexto = document.querySelector("#caixaTexto");
 const botao = document.querySelector("#botao-enviar");
-let mensagem = document.querySelector("#mensagem");
+const mensagem = document.querySelector("#mensagem");
 
+const areaTextoIdade = document.querySelector("#caixaTexto-idade");
 const botaoIdade = document.querySelector("#botao-enviar-idade");
-let mensagemIdade = document.querySelector("#mensagem-idade");
+const mensagemIdade = document.querySelector("#mensagem-idade");
 
-console.log("Estou funcionando!");
 
 botao.addEventListener("click", function() {
-    mensagem.textContent = "eu funciono";
+    console.log(areaTexto.value);
+        mensagem.textContent = "olá, " + areaTexto.value + "!";
 });
 
 botaoIdade.addEventListener("click", function() {
-    mensagemIdade.textContent = "eu também funciono";
+    console.log(areaTextoIdade.value);
+    mensagemIdade.textContent = "Você tem " + areaTextoIdade.value + " anos de idade!";
 });
